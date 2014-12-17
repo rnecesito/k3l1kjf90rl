aa3155abcc5b42cd3aa0edc47139282c8ad71253
Rey Necesito <reynecesito@gmail.com>
2014-12-17 20:09:04 +0800

    Merge from master

---
+++ app/src/main/java/com/example/sgm/japgolfapp/counting/BetCountingFragment.java
@@ -0,0 +1,146 @@
+package com.example.sgm.japgolfapp.counting;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+import com.example.sgm.japgolfapp.BaseFragment;
+import com.example.sgm.japgolfapp.R;
+import com.example.sgm.japgolfapp.settings.MenuSettingsFragment;
+
+import butterknife.OnClick;
+
+/**
+ * A simple {@link android.app.Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link com.example.sgm.japgolfapp.counting.BetCountingFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link com.example.sgm.japgolfapp.counting.BetCountingFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class BetCountingFragment extends BaseFragment {
+    View view_container;
+
+    boolean shown = false;
+
+    @OnClick(R.id.menu_button)
+    public void showMenu() {
+        LayoutInflater inflater = LayoutInflater.from(getActivity());
+        RelativeLayout rl = (RelativeLayout) view_container.findViewById(R.id.new_registration_main);
+        View item = inflater.inflate(R.layout.side_menu, rl, false);
+        if (!shown) {
+            item.setTag("side_menu_tag");
+            rl.addView(item);
+            SlideToRight(item);
+            Button settingButton = (Button)item.findViewById(R.id.settingButton);
+            settingButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    showFragmentAndAddToBackStack(new MenuSettingsFragment());
+                }
+            });
+            shown = true;
+        } else {
+            item = view_container.findViewWithTag("side_menu_tag");
+            SlideToLeft(item);
+            rl.removeView(item);
+            shown = false;
+        }
+
+        Button countingButton= (Button)item.findViewById(R.id.countingButton);
+        countingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LayoutInflater inflater = LayoutInflater.from(getContext());
+                RelativeLayout rl = (RelativeLayout) view_container.findViewById(R.id.new_registration_main);
+                final View item = inflater.inflate(R.layout.counting_sub_menu, rl, false);
+                item.setTag("counting_sub_menu");
+                View tagged = view_container.findViewWithTag("counting_sub_menu");
+                if(tagged == null) {
+                    rl.addView(item);
+                }
+//                showFragmentAndAddToBackStack(new MenuSettingsFragment());
+            }
+        });
+    }
+
+    public void SlideToRight(View view) {
+        Animation slide = null;
+        slide = new TranslateAnimation(Animation.RELATIVE_TO_SELF, -5.0f,
+                Animation.RELATIVE_TO_SELF, 0.0f, Animation.RELATIVE_TO_SELF,
+                0.0f, Animation.RELATIVE_TO_SELF, 0.0f);
+
+        slide.setDuration(400);
+        slide.setFillAfter(true);
+        slide.setFillEnabled(true);
+        view.startAnimation(slide);
+
+        slide.setAnimationListener(new Animation.AnimationListener() {
+
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+            }
+
+        });
+
+    }
+
+    public void SlideToLeft(View view) {
+        Animation slide = null;
+        slide = new TranslateAnimation(Animation.RELATIVE_TO_SELF, 0.0f,
+                Animation.RELATIVE_TO_SELF, -5.0f, Animation.RELATIVE_TO_SELF,
+                0.0f, Animation.RELATIVE_TO_SELF, 0.0f);
+
+        slide.setDuration(400);
+        slide.setFillAfter(true);
+        slide.setFillEnabled(true);
+        view.startAnimation(slide);
+
+        slide.setAnimationListener(new Animation.AnimationListener() {
+
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+            }
+
+        });
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_bet_counting, container, false);
+    }
+
+    @Override
+    public void onViewCreated(final View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        view_container = view;
+    }
+
+}
