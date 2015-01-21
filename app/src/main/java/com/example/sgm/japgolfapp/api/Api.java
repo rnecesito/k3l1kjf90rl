11eeb3173b5ad9735bd211a8ed59284923648907
Rey Necesito <reynecesito@gmail.com>
2015-01-21 17:13:08 +0800

    API update

--- app/src/main/java/com/example/sgm/japgolfapp/api/Api.java
+++ app/src/main/java/com/example/sgm/japgolfapp/api/Api.java
@@ -2,6 +2,8 @@ package com.example.sgm.japgolfapp.api;
 
 import android.content.Context;
 
+import org.apache.http.protocol.HTTP;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -14,7 +16,7 @@ import java.io.IOException;
 public class Api {
 
     public static String WEB_URL = "http://zoogtech.com/golfapp/public/";
-    public static final String CHARSET = "UTF-8";
+    public static final String CHARSET = HTTP.UTF_8;
     public static final String AUTHORIZATION = "Authorization";
     public static final String BEARER = "Bearer ";
 
